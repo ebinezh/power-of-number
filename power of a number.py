10e01da1b2b3ef520e7069401e6cedf439b46e95
ebinezh <88050562+ebinezh@users.noreply.github.com>
2021-08-02 21:15:27 +0530

    Add files via upload

---
+++ power of a number.py
@@ -0,0 +1,12 @@
+# power of a number
+n=float(input("Enter a number:"))
+e=int(input("Enter exponent: "))
+
+for i in range(1, e):
+    x=n**e
+print(n, "^", e, "=", x)
+
+
+# thanks for watching
+# like, share & subscribe
+# Dream2code
